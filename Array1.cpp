a2208c323911dff30bb3e3ee03ddc9d3bdfeb76d
dazzle-sah <drishti.sah1412@gmail.com>
2023-02-08 11:23:34 +0530

    Array Dec

---
+++ Array1.cpp
@@ -0,0 +1,19 @@
+
+ //Array with fixed size
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+ //Array Declaration
+ 
+ int a1[5];
+ int a2[]={1,3,5,7,9,2,4,6,8};
+ int a3[15]={2,4,6,8};
+ 
+}
+
+///////////////////////////////////////////////////////////////
