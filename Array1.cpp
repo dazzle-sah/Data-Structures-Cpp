091fcf141afed0873a0231dc54fd1931de00a80e
dazzle-sah <drishti.sah1412@gmail.com>
2023-02-08 11:28:15 +0530

    array ini

--- Array1.cpp
+++ Array1.cpp
@@ -5,15 +5,38 @@
 #include <string>
 using namespace std;
 
+// int main()
+// {
+//  //Array Declaration
+//  int a1[5];
+//  int a2[]={1,3,5,7,9,2,4,6,8};
+//  int a3[15]={2,4,6,8};
+// }
+
+
+//------------------------------------------------------------------------
+
 int main()
+
 {
 
- //Array Declaration
- 
+ //Array Initialisation 
+
  int a1[5];
  int a2[]={1,3,5,7,9,2,4,6,8};
  int a3[15]={2,4,6,8};
- 
-}
 
-///////////////////////////////////////////////////////////////
+ for(int i=0;i<5;i++){
+    cin>>a1[i];
+    cout<<a1[i];
+ }
+
+  ////////////////////////
+
+  int n;
+ cin>>n;
+  for(int i=0;i<5;i++){
+    cin>>a1[i];
+    cout<<a1[i];
+ }
+}
