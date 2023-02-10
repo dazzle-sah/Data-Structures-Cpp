1f9dbfde3f7bc9d5aa1c81544f860bffcec0825b
dazzle-sah <drishti.sah1412@gmail.com>
2023-02-10 14:22:13 +0530

    header

--- Linear Data Structures/Vector/modifiers.cpp
+++ Linear Data Structures/Vector/modifiers.cpp
@@ -1,12 +1,16 @@
 
-MODIFIERS -
+// MODIFIERS -
 
-assign() – It assigns new value to the vector elements by replacing old ones
-push_back() – It push the elements into a vector from the back
-pop_back() – It is used to pop or remove elements from a vector from the back.
-insert() – It inserts new elements before the element at the specified position
-erase() – It is used to remove elements from a container from the specified position or range.
-swap() – It is used to swap the contents of one vector with another vector of same type. Sizes may differ.
-clear() – It is used to remove all the elements of the vector container
-emplace() – It extends the container by inserting new element at position
-emplace_back() – It is used to insert a new element into the vector container, the new element is added to the end of the vector
+// assign() – It assigns new value to the vector elements by replacing old ones
+// push_back() – It push the elements into a vector from the back
+// pop_back() – It is used to pop or remove elements from a vector from the back.
+// insert() – It inserts new elements before the element at the specified position
+// erase() – It is used to remove elements from a container from the specified position or range.
+// swap() – It is used to swap the contents of one vector with another vector of same type. Sizes may differ.
+// clear() – It is used to remove all the elements of the vector container
+// emplace() – It extends the container by inserting new element at position
+// emplace_back() – It is used to insert a new element into the vector container, the new element is added to the end of the vector
+
+#include <iostream>
+#include <vector>
+using namespace std;
