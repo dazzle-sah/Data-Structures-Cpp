0da3aa5c239e65f97cc6d46a98cabd3edf5632a4
dazzle-sah <drishti.sah1412@gmail.com>
2023-02-10 14:23:25 +0530

    swapping

--- Linear Data Structures/Vector/modifiers.cpp
+++ Linear Data Structures/Vector/modifiers.cpp
@@ -62,3 +62,30 @@ int main()
     // erases the vector
     v.clear();
     cout << "\nVector size after erase(): " << v.size();
+
+     // two vector to perform swap
+    vector<int> v1, v2;
+    v1.push_back(1);
+    v1.push_back(2);
+    v2.push_back(3);
+    v2.push_back(4);
+ 
+    cout << "\n\nVector 1: ";
+    for (int i = 0; i < v1.size(); i++)
+        cout << v1[i] << " ";
+ 
+    cout << "\nVector 2: ";
+    for (int i = 0; i < v2.size(); i++)
+        cout << v2[i] << " ";
+ 
+    // Swaps v1 and v2
+    v1.swap(v2);
+ 
+    cout << "\nAfter Swap \nVector 1: ";
+    for (int i = 0; i < v1.size(); i++)
+        cout << v1[i] << " ";
+ 
+    cout << "\nVector 2: ";
+    for (int i = 0; i < v2.size(); i++)
+        cout << v2[i] << " ";
+}
