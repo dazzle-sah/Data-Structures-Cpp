9d96534f3dc0eaffdf3c2c421cda901336677fe2
dazzle-sah <drishti.sah1412@gmail.com>
2023-02-10 14:22:53 +0530

    one vector

--- Linear Data Structures/Vector/modifiers.cpp
+++ Linear Data Structures/Vector/modifiers.cpp
@@ -14,3 +14,51 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+int main()
+{
+    // Assign vector
+    vector<int> v;
+ 
+    // fill the vector with 10 five times
+    v.assign(5, 10);
+ 
+    cout << "The vector elements are: ";
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+ 
+    // inserts 15 to the last position
+    v.push_back(15);
+    int n = v.size();
+    cout << "\nThe last element is: " << v[n - 1];
+ 
+    // removes last element
+    v.pop_back();
+ 
+    // prints the vector
+    cout << "\nThe vector elements are: ";
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+ 
+    // inserts 5 at the beginning
+    v.insert(v.begin(), 5);
+ 
+    cout << "\nThe first element is: " << v[0];
+ 
+    // removes the first element
+    v.erase(v.begin());
+ 
+    cout << "\nThe first element is: " << v[0];
+ 
+    // inserts at the beginning
+    v.emplace(v.begin(), 5);
+    cout << "\nThe first element is: " << v[0];
+ 
+    // Inserts 20 at the end
+    v.emplace_back(20);
+    n = v.size();
+    cout << "\nThe last element is: " << v[n - 1];
+ 
+    // erases the vector
+    v.clear();
+    cout << "\nVector size after erase(): " << v.size();
